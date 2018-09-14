8525c6cf173ec7e3acf8babcf5066c5e9ff5778b
tinowells <43014245+tinowells@users.noreply.github.com>
2018-09-14 10:17:50 -1000

    Add files via upload
    today's updates

--- template1.py
+++ template1.py
@@ -20,26 +20,33 @@ tdata = tfile[0].data
 
 ## Extracting dimensions:
 d1,d2,d3 = tdata.shape[0],tdata.shape[1],tdata.shape[2] ## d1,d2,d3==499000,1,32 or something along those lines
+xidx = np.arange(0,d1,1) ## Setting x-axis to integer
+ymed = np.empty(d1)
+yavg = np.empty(d1)
+for ii in range(d1):
+    ymed[ii] = np.median(tdata[ii,:])
+    yavg[ii] = np.mean(tdata[ii,:])
+    if ii%1000==0:
+        print("\tii:\t{}".format(ii))
 
-## Printing dimesnions:
-see_data = input("Would you like to print data? (y or n): ")
-
-if see_data=='y':
-    for ii in range(d1):
-        print("ii:\t{0}\n\n{1}".format(ii,tdata[ii]))
-else:
-    print(' ')
-
-nave = np.empty(d3)
-for ii in range(d3):
-    randint = np.random.randint(0,d1)
-    print("Random int selected: {}".format(randint))
-    plt.plot(tdata[randint,0,:],linewidth=0.5)
-    nave[ii] = np.average(tdata[:,0,ii])
-    
-
-plt.plot(nave,".-",c='r',label="$\overline{nData}$",linewidth=0.5)
+error = np.abs(ymed-yavg)
 pdb.set_trace()
+plt.plot(xidx,ymed,linewidth=0.1,c='b',label='Median')
+plt.plot(xidx,yavg,linewidth=0.1,c='g',label='Mean')
+
+plterror = input("\n\tPlot Error? (y or n)")
+if plterror=='y':
+    plt.plot(xidx,error,linewidth=0.1,c='r',label='$\delta$')
+#plt.plot(xidx,np.median(yaxis),linewidth=0.5,c='r',label='Median')
+plt.xlabel("Index (increment of n==1)")
+#plt.ylabel("np.median(data[ii,:])")
 plt.legend()
 plt.show()
 
+
+
+
+
+
+
+pdb.set_trace() #Good habit to have set_trace()
