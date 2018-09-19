c824909b5cd5d98e84a09f3d0abc0f42884b1929
tinowells <43014245+tinowells@users.noreply.github.com>
2018-09-19 11:54:38 -1000

    Add files via upload
    9/19 update

--- template1.py
+++ template1.py
@@ -5,14 +5,16 @@ import numpy as np
 import pdb
 from astropy.io import fits
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 ## Constants:
-
+nframe_avg = 32
+plusminus = int(nframe_avg/2) ## for "plus or minus" in looping
+dfile='testfile.fits'
+nsubplots = 4
 
 
 ## Code:
-
-dfile='testfile.fits'
 tfile = fits.open(dfile)
 
 ## Semi-assuming were working with a data from of 1 (technically zero; check with tdata.info() should only have one row)
@@ -21,32 +23,40 @@ tdata = tfile[0].data
 ## Extracting dimensions:
 d1,d2,d3 = tdata.shape[0],tdata.shape[1],tdata.shape[2] ## d1,d2,d3==499000,1,32 or something along those lines
 xidx = np.arange(0,d1,1) ## Setting x-axis to integer
-ymed = np.empty(d1)
-yavg = np.empty(d1)
-for ii in range(d1):
-    ymed[ii] = np.median(tdata[ii,:])
-    yavg[ii] = np.mean(tdata[ii,:])
-    if ii%1000==0:
-        print("\tii:\t{}".format(ii))
+frame_med_norm = np.empty(d1)
 
-error = np.abs(ymed-yavg)
-pdb.set_trace()
-plt.plot(xidx,ymed,linewidth=0.1,c='b',label='Median')
-plt.plot(xidx,yavg,linewidth=0.1,c='g',label='Mean')
-
-plterror = input("\n\tPlot Error? (y or n)")
-if plterror=='y':
-    plt.plot(xidx,error,linewidth=0.1,c='r',label='$\delta$')
-#plt.plot(xidx,np.median(yaxis),linewidth=0.5,c='r',label='Median')
-plt.xlabel("Index (increment of n==1)")
-#plt.ylabel("np.median(data[ii,:])")
-plt.legend()
+for ii in range(0,d1-1000,1):
+    ## Extracting array dimensions for easier calculations and cleaner code
+    frame_med_norm[ii] = np.median(tdata[ii,:,:])-np.median(tdata[ii+1000,:,:])
+    if ii%10000==0:
+        print("\tii:\t{} / {}".format(ii,d1))
+gs = gridspec.GridSpec(nrows=nsubplots,ncols=1)
+for ii in range(0,nsubplots,1):
+    ax = plt.subplot(gs[ii,0])
+    plt.plot(frame_med_norm[int(ii*d1/nsubplots):int((ii+1)*(d1/nsubplots))],linewidth=0.3,c='k')
+    plt.xlim(left=0,right=d1/nsubplots)
+    if ii != nsubplots-1:
+        ax.xaxis.set_visible(False)
+plt.subplots_adjust(hspace=0)
+plt.savefig('normdata.pdf')
 plt.show()
 
+frame_reduced = np.zeros(d1)
 
+## Starting loop at p_m; throw away edges of plot
+for frame_set in range(plusminus,d1-plusminus,1):
+    frame_reduced[frame_set] = np.median(frame_med_norm[frame_set-plusminus:frame_set+plusminus-1])
+    if frame_set%10000==0:
+        print("\tframe_set:\t{} / {}".format(frame_set,d1-plusminus))
+plt.clf()
+for ii in range(0,nsubplots,1):
+    ax = plt.subplot(gs[ii,0])
+    plt.plot(frame_reduced[int(ii*d1/nsubplots):int((ii+1)*(d1/nsubplots))],linewidth=0.3,c='k')
+    plt.xlim(left=0,right=d1/nsubplots)
+    if ii != nsubplots-1:
+        ax.xaxis.set_visible(False)
+plt.subplots_adjust(hspace=0)
 
-
-
-
-
+plt.savefig('reddata.pdf')
+plt.show()
 pdb.set_trace() #Good habit to have set_trace()
