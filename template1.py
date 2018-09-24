3d0a8df1d0a1cb2a8d27957d2739fda206515d4b
tinowells <43014245+tinowells@users.noreply.github.com>
2018-09-24 12:00:53 -1000

    Add files via upload

--- template1.py
+++ template1.py
@@ -10,53 +10,66 @@ import matplotlib.gridspec as gridspec
 ## Constants:
 nframe_avg = 32
 plusminus = int(nframe_avg/2) ## for "plus or minus" in looping
-dfile='testfile.fits'
+dfile='180921_FrequencyResponse_64x256_2Vbias_10000f_cube.fits'
 nsubplots = 4
 
 
+
 ## Code:
-tfile = fits.open(dfile)
+hdulist = fits.open(dfile)
 
-## Semi-assuming were working with a data from of 1 (technically zero; check with tdata.info() should only have one row)
-tdata = tfile[0].data
-
+## Semi-assuming were working with a data from of 1 (technically zero; check with hdulist.info() should only have one row)
+tdata = hdulist[0].data
+pdb.set_trace()
 ## Extracting dimensions:
 d1,d2,d3 = tdata.shape[0],tdata.shape[1],tdata.shape[2] ## d1,d2,d3==499000,1,32 or something along those lines
 xidx = np.arange(0,d1,1) ## Setting x-axis to integer
 frame_med_norm = np.empty(d1)
 
-for ii in range(0,d1-1000,1):
+## (10000,256,64) - extract last :32 and plot
+
+#plt.clf()
+#for ii in range(0,d1,1):
+#    plt.plot(xidx[ii],np.median(tdata[ii,:,:]),'.',c='r')
+#    if ii%100==0:
+#        print("ii == {} \t\t {}".format(ii,np.median(tdata[ii,:,:])))
+#plt.show()
+#pdb.set_trace()
+
+for ii in range(0,d1-10,1):
     ## Extracting array dimensions for easier calculations and cleaner code
-    frame_med_norm[ii] = np.median(tdata[ii,:,:])-np.median(tdata[ii+1000,:,:])
-    if ii%10000==0:
+    frame_med_norm[ii] = np.median(tdata[ii,:,:])-np.median(tdata[ii+10,:,:])
+    ## plot raw values
+    if ii%1000==0:
         print("\tii:\t{} / {}".format(ii,d1))
 gs = gridspec.GridSpec(nrows=nsubplots,ncols=1)
-for ii in range(0,nsubplots,1):
-    ax = plt.subplot(gs[ii,0])
-    plt.plot(frame_med_norm[int(ii*d1/nsubplots):int((ii+1)*(d1/nsubplots))],linewidth=0.3,c='k')
-    plt.xlim(left=0,right=d1/nsubplots)
-    if ii != nsubplots-1:
-        ax.xaxis.set_visible(False)
-plt.subplots_adjust(hspace=0)
-plt.savefig('normdata.pdf')
-plt.show()
+#for ii in range(0,nsubplots,1):
+#    ax = plt.subplot(gs[ii,0])
+#    plt.plot(frame_med_norm[int(ii*d1/nsubplots):int((ii+1)*(d1/nsubplots))],linewidth=0.3,c='k')
+#    plt.xlim(left=0,right=d1/nsubplots)
+#    if ii != nsubplots-1:
+#        ax.xaxis.set_visible(False)
+#plt.subplots_adjust(hspace=0)
+#plt.savefig('normdata.pdf')
+#plt.show()
 
 frame_reduced = np.zeros(d1)
 
 ## Starting loop at p_m; throw away edges of plot
 for frame_set in range(plusminus,d1-plusminus,1):
-    frame_reduced[frame_set] = np.median(frame_med_norm[frame_set-plusminus:frame_set+plusminus-1])
-    if frame_set%10000==0:
+    frame_reduced[frame_set] = np.mean(frame_med_norm[frame_set-plusminus:frame_set+plusminus-1])
+    if frame_set%1000==0:
         print("\tframe_set:\t{} / {}".format(frame_set,d1-plusminus))
 plt.clf()
 for ii in range(0,nsubplots,1):
     ax = plt.subplot(gs[ii,0])
-    plt.plot(frame_reduced[int(ii*d1/nsubplots):int((ii+1)*(d1/nsubplots))],linewidth=0.3,c='k')
+    plt.plot(frame_med_norm[int(ii*d1/nsubplots):int((ii+1)*(d1/nsubplots))],linewidth=0.3,c='r',label='Normalized')
+    plt.plot(frame_reduced[int(ii*d1/nsubplots):int((ii+1)*(d1/nsubplots))],linewidth=0.5,c='k',label='Reduced')
     plt.xlim(left=0,right=d1/nsubplots)
     if ii != nsubplots-1:
         ax.xaxis.set_visible(False)
 plt.subplots_adjust(hspace=0)
-
-plt.savefig('reddata.pdf')
+plt.legend()
+plt.savefig('nord_reduced_data.pdf')
 plt.show()
 pdb.set_trace() #Good habit to have set_trace()
