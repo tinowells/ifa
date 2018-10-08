557f8e5075b3ac0583f4b30b4e09447ae2ea8893
tinowells <43014245+tinowells@users.noreply.github.com>
2018-10-08 12:25:34 -1000

    Add files via upload
    10/08/2018 code updates and figures. Code takes approx. 14 minutes to run on Zamboni, work on making it run faster.

--- template1.py
+++ template1.py
@@ -3,6 +3,7 @@
 ## Packages:
 import numpy as np
 import pdb
+import time
 from astropy.io import fits
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
@@ -23,45 +24,45 @@ tdata = hdulist[0].data
 ## Extracting dimensions:
 d1,d2,d3 = tdata.shape[0],tdata.shape[1],tdata.shape[2] ## d1,d2,d3==499000,1,32 or something along those lines
 xidx = np.arange(0,d1,1) ## Setting x-axis to integer
+
 frame_med_norm = np.empty(d1)
-frame_row_med = np.empty((d1,d2))
-
-## Set_trace() here to check new data shape
-pdb.set_trace()
-
-## each row of 64, median to get a 256 set of medians for each frame
-## subtract 265-n+1 from 265-n frame, then average
-
-
+print("Normalizing Data...")
 for ii in range(0,d1-1,1):
+    print('\t\tNormalizing Iteration: {} / {}'.format(ii,d1-1))
     ## Extracting array dimensions for easier calculations and cleaner code
     frame_med_norm[ii] = np.median(tdata[ii,:,:])-np.median(tdata[ii+1,:,:])
-    for jj in range(0,d2-1,1):
-        frame_row_med[ii,jj] = np.mean(tdata[ii,jj,:])
+print("\t...Data normalized.")
 
-    ## plot raw values
-    if ii%100==0:
-        print("\tii:\t{} / {}".format(ii,d1))
+frame_reduced = np.zeros(d1)
+frameavg = np.zeros((d1,d2,d3)) ## Extracting/duplicating frameset and size
+print("Reducing Data ... ")
+t0 = time.time()
+tt = np.zeros(d1-plusminus)
+for nframe in range(plusminus,d1-plusminus,1):
+    if nframe%10==0:
+        t1 = time.time()
+        tt[nframe] = t1-t0
+        print("\tIteration: {} / {} \t\t ETR: {} seconds".format(nframe,d1-plusminus,int((tt[nframe])*(d1-nframe-plusminus)/10)))
+        t0 = time.time()
+    frame_reduced[nframe] = np.mean(frame_med_norm[nframe-plusminus:nframe+plusminus])
+    for jj in range(d2):
+        for kk in range(d3):
+            frameavg[nframe,jj,kk] = np.mean(tdata[nframe-plusminus:nframe+plusminus,jj,kk])
 
-tino = np.array([frame_row_med[ii]-frame_row_med[ii+1] for ii in range(0,d1-1,1)])
-tinoavg = np.array([np.median(tino[zz-plusminus:zz+plusminus-1]) for zz in range(plusminus,d1-plusminus+1,1)])
-for ii in range(plusminus): ## must insert first plusminus indicies as zeros or data does not overlap with reduced
-    tinoavg = np.insert(tinoavg,ii,0.)
+red2 = np.zeros(d1)
+for nframe in range(0,d1-1,1):
+    red2[nframe] = np.median(frameavg[nframe])-np.median(frameavg[nframe+1])
+print("\t...Data reduced.")
 
+print('... Plotting ...')
 plt.clf()
 gs = gridspec.GridSpec(nrows=nsubplots,ncols=1)
-frame_reduced = np.zeros(d1)
-## Starting loop at p_m; throw away edges of plot
-for frame_set in range(plusminus,d1-plusminus,1):
-    frame_reduced[frame_set] = np.mean(frame_med_norm[frame_set-plusminus:frame_set+plusminus-1])
-    if frame_set%100==0:
-        print("\tframe_set:\t{} / {}".format(frame_set,d1-plusminus))
-plt.clf()
+
 for ii in range(0,nsubplots,1):
     ax = plt.subplot(gs[ii,0])
     plt.plot(frame_med_norm[int(ii*d1/nsubplots):int((ii+1)*(d1/nsubplots))],linewidth=0.2,c='r',label='Normalized')
     plt.plot(frame_reduced[int(ii*d1/nsubplots):int((ii+1)*(d1/nsubplots))],linewidth=0.5,c='k',label='Reduced')
-    #plt.plot(tinoavg[int(ii*d1/nsubplots):int((ii+1)*(d1/nsubplots))],linewidth=0.5,c='b',label='New')
+    plt.plot(red2[int(ii*d1/nsubplots):int((ii+1)*(d1/nsubplots))],linewidth=0.5,c='b',label='Red2')
 
     plt.xlim(left=0,right=d1/nsubplots)
     if ii != nsubplots-1:
@@ -69,7 +70,5 @@ for ii in range(0,nsubplots,1):
 plt.subplots_adjust(wspace=0,hspace=0,top=1.,bottom=0.026,left=0.026,right=0.989)
 
 plt.legend()
-#plt.tight_layout()
 plt.savefig('{}_normrednew.pdf'.format(dfile[:-5])) ## Cutting off *.fits extension
 plt.show()
-##pdb.set_trace() #Good habit to have set_trace()
