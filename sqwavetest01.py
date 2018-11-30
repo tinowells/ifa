63241337d700189bd12ed5c85ab512ccb10a0ebf
tinowells <43014245+tinowells@users.noreply.github.com>
2018-11-30 09:21:51 -1000

    11/30/18 Weekly update
    Zip folder containing all figures form 181119 (29-32) data, reduction package using mean, not median. 
    Screen shot shows LED-onset/-offset perfectly identified (a win for the software)
    Code is update - requires fits file in script call

--- sqwavetest01.py
+++ sqwavetest01.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 import pdb
+import os
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
+#from scipy.stats import signaltonoise as snr
 from astropy.io import fits
 import pandas as pd
 import time
@@ -12,36 +14,32 @@ import matplotlib.gridspec as gridspec
 from astropy.io import fits
 import argparse
 
+## if peaks are not detected correctly, redefine tolerance parameter
+
 clrlst = ['b','r','g','k','deeppink','cyan','magenta']
+xlbl = 'Frame(n)'
+ylbl = '$\Delta$ ADU'
 
 parser = argparse.ArgumentParser(description='Frequency Response Reduction & Ramp Extraction Plots')
 parser.add_argument('-fits','--fitsfile',type=str,metavar='',required=True,help='Fits File you would like to reduce and plot.')
-parser.add_argument('-tol','--peak_tolerance',type=int,metavar='',required=False,help='The amount of tolerance between frame n and frame n+npeaksteps to find peak')
-parser.add_argument('-nsteps','--nsteps',metavar='',required=False,type=int,help='How many steps to loo ahead when trying to find a peak')
-parser.add_argument('-save','--savefigures',metavar='',required=False,type=bool,help='True if you would like to save figures, False is you dont. Defaults to False.')
-parser.add_argument('-write','--writeout',metavar='',required=False,type=bool,help='True if you want full statistics textfile on code and data, False if you dont. Defaults to False.')
+parser.add_argument('-tol','--peak_tolerance',type=int,metavar='',required=False,help='The amount of tolerance between frame n and frame n+npeaksteps to find peak',default=5)
+parser.add_argument('-nsteps','--nsteps',metavar='',required=False,type=int,help='How many steps to loo ahead when trying to find a peak',default=50)
+parser.add_argument('-save','--savefigures',metavar='',required=False,type=bool,help='True if you would like to save figures, False is you dont. Defaults to False.',default=False)
+parser.add_argument('-write','--writeout',metavar='',required=False,type=bool,help='True if you want full statistics textfile on code and data, False if you dont. Defaults to False.',default=False)
 args = parser.parse_args()
 
 ## Handling optional arguments:
+print("\n Parsing arguments...")
 dfile = args.fitsfile
-if args.peak_tolerance is None:
-    tolerance = 5
-else:
-    tolerance = args.peak_tolerance
-if args.nsteps is None:
-    npeaksteps = 50
-else:
-    npeaksteps = args.nsteps
-if args.savefigures is None:
-    savefigs = False
-else:
-    savefigs = True
-    savefilename = dfile[0:7] ## just the date and _
-if args.writeout is None:
-    writefile = False
-else:
-    writefile = True
+tolerance = args.peak_tolerance
+npeaksteps = args.nsteps
+savefigs = args.savefigures
+if savefigs:
+    savefilename = dfile[0:7]
+writefile = args.writeout
+if writefile:
     txtfile = '{}.txt'.format(dfile[0:7])
+print("\t...arguments parsed.")
 
 def read_reduce(dfile):
     print(' Reducing data ...')
@@ -56,7 +54,7 @@ def read_reduce(dfile):
     ## Running CDS(1,1) reduction:
     for ii in range(0,d0-1,1):
         temp = fdata[ii+1,:,:]-fdata[ii,:,:]
-        temp_med = np.median(temp)
+        temp_med = np.mean(temp)
         reduced_subframe.append(temp_med)
     dataframe = pd.DataFrame(reduced_subframe,columns=['adu'])
     print('\t... data reduced.')
@@ -72,7 +70,7 @@ def compress_df(dframe):
 def detect_peaks(data,npeakstep=npeaksteps):
     print(' Detecting peaks ...')
     onidx = []
-    for ii in range(0,len(data)-npeakstep,1):
+    for ii in range(16,len(data)-npeakstep,1):
         dadu = np.abs(data[ii])-np.abs(data[ii+npeakstep])
         if np.abs(dadu) >= tolerance and dadu > 0.:
             ## this also identifies random peaks, so we need to get rid of that
@@ -90,18 +88,44 @@ def check_peakidx(data,peaks):
     peak_idx = []
     idx_bool = np.diff(peaks) >= 350
     idx = np.where(idx_bool==True)[0].tolist()
+    #pdb.set_trace()
     for ii in range(len(idx)):
         peak_idx.append(peaks[idx[ii]])
     print('\t... outlier peaks extracted.')
-    return peak_idx
+    adjusted_peaks = refine_peaks(data,peak_idx)
+    return adjusted_peaks
+
+def refine_peaks(data,peaks,ledoffset=True):
+    print(" Refining peaks ...")
+    adjusted_peaks = []
+    for ii in range(len(peaks)):
+        idx = peaks[ii]
+        nadjust = 2
+        dadu_down = False
+        while dadu_down == False:
+            temp_data = data[idx-nadjust:idx]
+            #pdb.set_trace()
+            if temp_data[0] >= temp_data[1]:
+                dadu_down = False
+                nadjust += 1
+            else: ## meaning the previous index has a lower delta ADU value, meaning the delta ADU isn't constantly decreasing starting at this index ... 
+                dadu_down = True
+                print("\tPeak {} adjusted from {} to {}".format(ii,idx,idx-nadjust))
+                adjusted_peaks.append(idx-nadjust)
+            if nadjust == 100:
+                print("Stuck in loop - restart program with more defined parameters for tolerance and npeaksteps")
+    print("\t...all peaks refined.")
+    return adjusted_peaks
 
 def get_ramp_idx(onidx):
     print(' Extracting ramp indicies...')
     rampidx = []
-    dramps = np.diff(onidx)[0].tolist()
-    rampoffset = int(np.median(dramps)/2)-50
-    for ii in range(len(onidx)):
-        rampidx.append(onidx[ii]+rampoffset)
+    rampoffset = []
+    dramps = np.diff(onidx).tolist()
+    #rampoffset = int(np.median(dramps)/2)
+    for ii in range(len(dramps)):
+        rampoffset.append(dramps[ii]/2)
+        rampidx.append(onidx[ii]+rampoffset[ii])
     print('\t... ramp indicies extracted.')
     return rampidx,rampoffset
 
@@ -112,38 +136,99 @@ def plot_data(data,offidx,ridx):
         plt.axvline(x=offidx[ii],color='r',linestyle='--')
     for jj in range(len(ridx)):
         plt.axvline(x=ridx[jj],color='g',linestyle='-.')
-    
     ## finding local max/mins for image formatting
     plt.ylim(top = np.max(data[int(len(data)*0.1):int(len(data)*0.9)]),bottom=np.min(data[int(len(data)*0.1):int(len(data)*0.9)]))
-    plt.savefig('{}.pdf'.format(dfile.replace('.csv','')))
+    plt.xlabel(xlbl)
+    plt.ylabel(ylbl)
+    plt.title(dfile)
+    plt.savefig("{}{}_full.pdf".format(dfile[0:7],dfile[-7:-5]))
     plt.show()
-    
+
     ## for ymax, median first half, for ymin median second hald 
     ## plotting all ramps on one subplot
-    for kk in range(len(ridx)-1):
+    ## if len(ridx) == 4, code only extracts three so implement conditional for exception handling
+    if len(ridx) > 4:
+        nsubplots = 4
+    else:
+        nsubplots = len(ridx)-1
+
+    for kk in range(nsubplots):
         ramp = data[int(ridx[kk]):int(ridx[kk+1])]
         plt.plot(ramp,c=clrlst[kk],linewidth=0.5,label='R{}'.format(kk))
+    ## plotting last ramp bc python loops up to but not including
+    lastramp = data[ridx[-1]:ridx[-1]+len(data[int(ridx[-2]):int(ridx[-1])])]
+    plt.plot(lastramp,c=clrlst[kk+1],linewidth=0.5,label="R{}".format(kk+1))
     ytop = np.median(ramp[:len(ramp)/2])+5
     ybottom = np.median(ramp[len(ramp)/2:])-5
     plt.ylim(top=ytop,bottom=ybottom)
     plt.legend(prop={'size':6})
+    plt.xlabel(xlbl)
+    plt.ylabel(ylbl)
+    plt.savefig("{}{}_ramps.pdf".format(dfile[0:7],dfile[-7:-5]))
     plt.show()
 
     ## plotting ramps as indicvidual subplots
     plt.clf()
-    gs = gridspec.GridSpec(nrows=len(ridx)-1,ncols=1)
-    for ii in range(len(ridx)-1):
+    plt.ylabel(ylbl)
+    gs = gridspec.GridSpec(nrows=nsubplots+1,ncols=1)
+    for ii in range(nsubplots):
         ax = plt.subplot(gs[ii,0])
         ramp = data[int(ridx[ii]):int(ridx[ii+1])]
         ytop = np.median(ramp[:len(ramp)/2])+5
         ybottom = np.median(ramp[len(ramp)/2:])-5
-        plt.plot(ramp,c=clrlst[ii],label='R{}'.format(ii))
+        plt.plot(ramp,c=clrlst[ii],linewidth=0.5,label='R{}'.format(ii))
         plt.ylim(top=ytop,bottom=ybottom)
         plt.legend(prop={'size': 6})
         if ii != len(ridx)-2:
             ax.xaxis.set_visible(False)
+    ## adding last subplot
+    ax = plt.subplot(gs[ii+1,0])
+    plt.plot(lastramp,linewidth=0.5,c=clrlst[ii+1],label="R{}".format(ii+1))
+    plt.legend(prop={'size': 6})
     plt.subplots_adjust(wspace=0,hspace=0,top=0.94,bottom=0.1,left=0.1,right=0.975)
+    plt.xlabel(xlbl)
+    plt.savefig("{}{}_subplots.pdf".format(dfile[0:7],dfile[-7:-5]))
+    #plt.ylabel(ylbl)
     plt.show()
+
+    ## plotting co-added ramps to dampen noise
+    minnramp = 2000
+    for ii in range(nsubplots): ## finding shortest ramp
+        ramp = data[int(ridx[ii]):int(ridx[ii+1])]
+        if len(ramp) < minnramp:
+            minnramp = len(ramp)
+    addramp = np.zeros(minnramp)
+    
+    for ii in range(nsubplots):
+        for jj in range(minnramp):
+            addramp[jj] += data[int(ridx[ii]):int(ridx[ii+1])][jj]
+    for ii in range(len(addramp)):
+        addramp[ii] += lastramp[ii]
+    plt.clf()
+    #pdb.set_trace()
+    ytop,ybottom = np.median(addramp[:len(addramp)/2])+10,np.median(addramp[len(addramp)/2:])-5
+    plt.plot(addramp,c='k',label="R0-R{} Co-Added".format(nsubplots))
+    plt.ylim(top=ytop,bottom=ybottom)
+    plt.xlabel(xlbl)
+    plt.ylabel(ylbl)
+    plt.legend(prop={'size':6})
+    plt.savefig("{}{}_coadded.pdf".format(dfile[0:7],dfile[-7:-5]))
+    plt.show()
+
+    for ii in range(len(addramp)):
+        addramp[ii] = addramp[ii]/(float(nsubplots)+1) ## to account for the added ramp
+    ytop = np.median(addramp[:len(addramp)/2])+5
+    ybottom = np.median(addramp[len(addramp)/2:])-5
+    plt.plot(addramp,c='k',label="Co-Added Mean")
+    plt.legend(prop={'size':6})
+    plt.title(dfile)
+    plt.ylim(top=ytop,bottom=ybottom)
+    plt.xlabel(xlbl)
+    plt.ylabel(ylbl)
+    plt.savefig("{}{}_coadded_mean.pdf".format(dfile[0:7],dfile[-7:-5]))
+    plt.show()
+
+
 
 if __name__=='__main__':
     ## read fits file, reduce data as CDS(1,1) and return pandas dataframe
@@ -153,3 +238,12 @@ if __name__=='__main__':
     offsetidx = check_peakidx(reduced_data,allpeakidx)
     ramp_idx,ramp_offset = get_ramp_idx(offsetidx)
     plot_data(reduced_data,offsetidx,ramp_idx)
+    #pdb.set_trace()
+
+    ## pylatex for documentation
+    ## SNR proportional root N 
+    ## second reduction pipeline w/ columns, then MCMC QAL-EQW-ABS 3-D density code
+
+
+    ## Need to add second refinement to peak detection
+    ## sec
